6d1e1b1ae3fde47bfca2abc807a5706129dc8ef4
khansquine <khansquine@gmail.com>
2022-08-25 16:29:31 -0400

    Data Table

---
+++ Table/Production.js
@@ -0,0 +1,107 @@
+var production = [
+    {
+        "Region": "Africa",
+        "Year": "1961",
+        "Tonnes": "1960373"
+    },
+    {
+        "Region": "Africa",
+        "Year": "2000",
+        "Tonnes": "4331042"
+    },
+    {
+        "Region": "Africa",
+        "Year": "2018",
+        "Tonnes": "7081479"
+    },
+    {
+        "Region": "Asia",
+        "Year": "1961",
+        "Tonnes": "2729264"
+    },
+    {
+        "Region": "Asia",
+        "Year": "2000",
+        "Tonnes": "13040793"
+    },
+    {
+        "Region": "Asia",
+        "Year": "2018",
+        "Tonnes": "18514995"
+    },
+    {
+        "Region": "Australia & New Zealand",
+        "Year": "1961",
+        "Tonnes": "879424"
+    },
+    {
+        "Region": "Australia & New Zealand",
+        "Year": "2000",
+        "Tonnes": "2575359"
+    },
+    {
+        "Region": "Australia & New Zealand",
+        "Year": "2018",
+        "Tonnes": "2895633"
+    },
+    {
+        "Region": "Central America",
+        "Year": "1961",
+        "Tonnes": "525710"
+    },
+    {
+        "Region": "Central America",
+        "Year": "2000",
+        "Tonnes": "1765838"
+    },
+    {
+        "Region": "Central America",
+        "Year": "2018",
+        "Tonnes": "2536210"
+    },
+    {
+        "Region": "Europe",
+        "Year": "1961",
+        "Tonnes": "9691644"
+    },
+    {
+        "Region": "Europe",
+        "Year": "2000",
+        "Tonnes": "11771044"
+    },
+    {
+        "Region": "Europe",
+        "Year": "2018",
+        "Tonnes": "10641599"
+    },
+    {
+        "Region": "Northern America",
+        "Year": "1961",
+        "Tonnes": "8081148"
+    },
+    {
+        "Region": "Northern America",
+        "Year": "2000",
+        "Tonnes": "13280048"
+    },
+    {
+        "Region": "Northern America",
+        "Year": "2018",
+        "Tonnes": "13450584"
+    },
+    {
+        "Region": "South America",
+        "Year": "1961",
+        "Tonnes": "4666466"
+    },
+    {
+        "Region": "South America",
+        "Year": "2000",
+        "Tonnes": "11846128"
+    },
+    {
+        "Region": "South America",
+        "Year": "2018",
+        "Tonnes": "16250766"
+    }
+]
