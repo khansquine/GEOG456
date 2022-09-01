4a9cf59a371cdf0498dd1e7626ddb0ad047bdf61
khansquine <khansquine@gmail.com>
2022-09-01 10:10:00 -0400

    NewData

--- Table/Production.js
+++ Table/Production.js
@@ -1,107 +1,235 @@
-var production = [
+var production = 
+[
     {
-        "Region": "Africa",
-        "Year": "1961",
-        "Tonnes": "1960373"
-    },
-    {
-        "Region": "Africa",
-        "Year": "2000",
-        "Tonnes": "4331042"
-    },
-    {
-        "Region": "Africa",
-        "Year": "2018",
-        "Tonnes": "7081479"
-    },
-    {
-        "Region": "Asia",
-        "Year": "1961",
-        "Tonnes": "2729264"
-    },
-    {
-        "Region": "Asia",
-        "Year": "2000",
-        "Tonnes": "13040793"
-    },
-    {
-        "Region": "Asia",
-        "Year": "2018",
-        "Tonnes": "18514995"
-    },
-    {
-        "Region": "Australia & New Zealand",
-        "Year": "1961",
-        "Tonnes": "879424"
-    },
-    {
-        "Region": "Australia & New Zealand",
-        "Year": "2000",
-        "Tonnes": "2575359"
-    },
-    {
-        "Region": "Australia & New Zealand",
-        "Year": "2018",
-        "Tonnes": "2895633"
-    },
-    {
-        "Region": "Central America",
-        "Year": "1961",
-        "Tonnes": "525710"
-    },
-    {
-        "Region": "Central America",
-        "Year": "2000",
-        "Tonnes": "1765838"
-    },
-    {
-        "Region": "Central America",
-        "Year": "2018",
-        "Tonnes": "2536210"
-    },
-    {
-        "Region": "Europe",
-        "Year": "1961",
-        "Tonnes": "9691644"
-    },
-    {
-        "Region": "Europe",
-        "Year": "2000",
-        "Tonnes": "11771044"
-    },
-    {
-        "Region": "Europe",
-        "Year": "2018",
-        "Tonnes": "10641599"
-    },
-    {
-        "Region": "Northern America",
-        "Year": "1961",
-        "Tonnes": "8081148"
-    },
-    {
-        "Region": "Northern America",
-        "Year": "2000",
-        "Tonnes": "13280048"
-    },
-    {
-        "Region": "Northern America",
-        "Year": "2018",
-        "Tonnes": "13450584"
-    },
-    {
-        "Region": "South America",
         "Year": "1961",
         "Tonnes": "4666466"
     },
     {
-        "Region": "South America",
+        "Year": "1962",
+        "Tonnes": "4925593"
+    },
+    {
+        "Year": "1963",
+        "Tonnes": "5200308"
+    },
+    {
+        "Year": "1964",
+        "Tonnes": "4799576"
+    },
+    {
+        "Year": "1965",
+        "Tonnes": "4778153"
+    },
+    {
+        "Year": "1966",
+        "Tonnes": "5005298"
+    },
+    {
+        "Year": "1967",
+        "Tonnes": "5276923"
+    },
+    {
+        "Year": "1968",
+        "Tonnes": "5626473"
+    },
+    {
+        "Year": "1969",
+        "Tonnes": "6126357"
+    },
+    {
+        "Year": "1970",
+        "Tonnes": "5977409"
+    },
+    {
+        "Year": "1971",
+        "Tonnes": "5240122"
+    },
+    {
+        "Year": "1972",
+        "Tonnes": "5671965"
+    },
+    {
+        "Year": "1973",
+        "Tonnes": "5703862"
+    },
+    {
+        "Year": "1974",
+        "Tonnes": "5708547"
+    },
+    {
+        "Year": "1975",
+        "Tonnes": "6216523"
+    },
+    {
+        "Year": "1976",
+        "Tonnes": "6935754"
+    },
+    {
+        "Year": "1977",
+        "Tonnes": "7051650"
+    },
+    {
+        "Year": "1978",
+        "Tonnes": "7489163"
+    },
+    {
+        "Year": "1979",
+        "Tonnes": "7411950"
+    },
+    {
+        "Year": "1980",
+        "Tonnes": "7507599"
+    },
+    {
+        "Year": "1981",
+        "Tonnes": "7857045"
+    },
+    {
+        "Year": "1982",
+        "Tonnes": "7499701"
+    },
+    {
+        "Year": "1983",
+        "Tonnes": "7648696"
+    },
+    {
+        "Year": "1984",
+        "Tonnes": "7775803"
+    },
+    {
+        "Year": "1985",
+        "Tonnes": "8178129"
+    },
+    {
+        "Year": "1986",
+        "Tonnes": "8434269"
+    },
+    {
+        "Year": "1987",
+        "Tonnes": "7989397"
+    },
+    {
+        "Year": "1988",
+        "Tonnes": "8496882"
+    },
+    {
+        "Year": "1989",
+        "Tonnes": "8919911"
+    },
+    {
+        "Year": "1990",
+        "Tonnes": "9368722"
+    },
+    {
+        "Year": "1991",
+        "Tonnes": "9597538"
+    },
+    {
+        "Year": "1992",
+        "Tonnes": "9578362"
+    },
+    {
+        "Year": "1993",
+        "Tonnes": "9739472"
+    },
+    {
+        "Year": "1994",
+        "Tonnes": "10078549"
+    },
+    {
+        "Year": "1995",
+        "Tonnes": "10604086"
+    },
+    {
+        "Year": "1996",
+        "Tonnes": "11237549"
+    },
+    {
+        "Year": "1997",
+        "Tonnes": "11178247"
+    },
+    {
+        "Year": "1998",
+        "Tonnes": "10808532"
+    },
+    {
+        "Year": "1999",
+        "Tonnes": "11587949"
+    },
+    {
         "Year": "2000",
         "Tonnes": "11846128"
     },
     {
-        "Region": "South America",
+        "Year": "2001",
+        "Tonnes": "11651386"
+    },
+    {
+        "Year": "2002",
+        "Tonnes": "12086162"
+    },
+    {
+        "Year": "2003",
+        "Tonnes": "12325121"
+    },
+    {
+        "Year": "2004",
+        "Tonnes": "13336823"
+    },
+    {
+        "Year": "2005",
+        "Tonnes": "14533987"
+    },
+    {
+        "Year": "2006",
+        "Tonnes": "15110539"
+    },
+    {
+        "Year": "2007",
+        "Tonnes": "15538287"
+    },
+    {
+        "Year": "2008",
+        "Tonnes": "15281210"
+    },
+    {
+        "Year": "2009",
+        "Tonnes": "15265235"
+    },
+    {
+        "Year": "2010",
+        "Tonnes": "14698231"
+    },
+    {
+        "Year": "2011",
+        "Tonnes": "14513371"
+    },
+    {
+        "Year": "2012",
+        "Tonnes": "14994690"
+    },
+    {
+        "Year": "2013",
+        "Tonnes": "15685487"
+    },
+    {
+        "Year": "2014",
+        "Tonnes": "15625830"
+    },
+    {
+        "Year": "2015",
+        "Tonnes": "15308584"
+    },
+    {
+        "Year": "2016",
+        "Tonnes": "15066457"
+    },
+    {
+        "Year": "2017",
+        "Tonnes": "15529460"
+    },
+    {
         "Year": "2018",
         "Tonnes": "16250766"
-    }
+    },
 ]
